d84878ed1128028f31aa0fdbf455ced43cd2e94a
Akira Chenyavanij <natersland@gmail.com>
2023-11-26 22:39:49 +0700

    fix: delete weird code

--- src/features/home/components/navigation_bar.tsx
+++ src/features/home/components/navigation_bar.tsx
@@ -3,9 +3,7 @@ import { useHomePageStore } from "../stores/home_page_store";
 import { Page } from "@/common/const/type";
 
 const NavigationBar = () => {
-  const setStateUpdatePage = useHomePageStore((state) => state.setPage);
-  const setIncrementPage = useHomePageStore((state) => state.setPage);
-  const setUseEffectPage = useHomePageStore((state) => state.setPage);
+  const setPageIndex = useHomePageStore((state) => state.setPage);
 
   const page = useHomePageStore((state) => state.page);
   const selectedStyle = "underline text-primary";
@@ -15,14 +13,14 @@ const NavigationBar = () => {
     <div className="flex flex-row">
       <Button
         variant="link"
-        onClick={() => setStateUpdatePage(Page.STATE_UPDATE)}
+        onClick={() => setPageIndex(Page.STATE_UPDATE)}
         className={page === Page.STATE_UPDATE ? selectedStyle : unselectedStyle}
       >
         01 State Update
       </Button>
       <Button
         variant="link"
-        onClick={() => setIncrementPage(Page.INCREMENT_DECREMENT)}
+        onClick={() => setPageIndex(Page.INCREMENT_DECREMENT)}
         className={
           page === Page.INCREMENT_DECREMENT ? selectedStyle : unselectedStyle
         }
@@ -31,7 +29,7 @@ const NavigationBar = () => {
       </Button>
       <Button
         variant="link"
-        onClick={() => setUseEffectPage(Page.USE_EFFECT)}
+        onClick={() => setPageIndex(Page.USE_EFFECT)}
         className={page === Page.USE_EFFECT ? selectedStyle : unselectedStyle}
       >
         03 Use Effect
